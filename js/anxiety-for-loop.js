a09c4b0135a2d8b165c93eae5708ab1bcd932456
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-08 13:37:41 -0500

    fully functioning anxiety loop

--- js/anxiety-for-loop.js
+++ js/anxiety-for-loop.js
@@ -1,15 +1,15 @@
-var anxietyLevel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//var anxietyLevel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for(var anxietyLevel = 0; anxietyLevel <= 10; anxietyLevel++) {
-    var anxietyPrompt = prompt("What is your current anxiety level?");
-    if (anxietyPrompt <= 4) {
+do {
+    anxietyLevel = prompt("What is your current anxiety level?");
+    if (anxietyLevel <= 4) {
         alert("Your anxiety level is : " + anxietyLevel + " Take a walk.");
-    } else if (anxietyPrompt <= 7) {
+    } else if (anxietyLevel <= 7) {
         alert("Your anxiety level is : " + anxietyLevel + " Take a pill and do some breathing exercises.");
     } else {
         alert("Your anxiety level is : " + anxietyLevel + " Time for a nap.")
         //this would work with a 2-3 hour nap and maybe a cup of tea
     }
 
-}
+}while (anxietyLevel <= 10);
 
