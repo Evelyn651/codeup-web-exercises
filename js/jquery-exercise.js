725e39caad242fe890ee613c9da0bf00623fcfe9
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-09 10:50:45 -0500

    exercise for jquery events

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,5 +1,23 @@
 "use strict";
 
+//$(document).ready(function(){
+ //   alert("The pase is ready");
+//});
+
+//Exercise using id selectors
+
 $(document).ready(function(){
-    alert("The pase is ready");
+    $('h1').click(function(){
+        $('h1').css('background-color', 'teal')
+    });
+    $('p').dblclick(function(){
+        $('p').css('font-size', '18px')
+    });
+    var onList = function(){
+        $('li').css('background-color', 'grey')
+    };
+    var offList = function(){
+        $('li').css('background-color', 'white')
+    };
+    $('li').hover(onList, offList);
 });
