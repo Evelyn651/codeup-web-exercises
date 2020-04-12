64939a42886a2d9b461f7fa363edbde6cdf5382e
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-11 22:19:16 -0500

    understanding how to write different functions

--- js/codecademy.js
+++ js/codecademy.js
@@ -1,8 +1,25 @@
-
+//7- Helper Functions
 function monitorCount(rows, columns) {
     return rows * columns;
 }
 
 function costOfMonitors(rows, columns){
-
+    return monitorCount(rows, columns) * 200;
 }
+var totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
+
+//8- Function Expressions
+
+var plantNeedsWater = function(day){
+    if(day === 'Wednesday'){
+        return true;
+    }else{
+        return false;
+    }
+};
+
+console.log(plantNeedsWater('Tuesday'));
+
+//9- Arrow Functions
+
