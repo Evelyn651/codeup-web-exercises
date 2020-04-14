6a0bfcc567b51ffe95c6e2742ed7c69f82f494b3
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-13 23:57:44 -0500

    feat: finished functions section

--- js/codecademy.js
+++ js/codecademy.js
@@ -35,4 +35,44 @@ var plantsNeedWater = (day) => {
 console.log(plantsNeedWater('Friday'));
 
 // 10- Concise Body Arrow Functions
+var plantsNeedWater = day => day ==='Wednesday' ? true : false;
 
+/* 11- Review
+A function is a reusable block of code that groups together a sequence of statements to perform a specific task.
+
+A parameter is a named variable inside a function's block which will be assigned the value of the argument passed in
+ when the function is invoked:
+
+ function calculateArea (width, height){
+            console.log(width * height);
+            }
+  Width and height are the parameters.
+
+ To call a function, use the name of the function with empty parenthesis.
+
+ To return a value, use a return statement
+
+ To define a function use function expressions:
+
+ e.g.
+ var calculateArea[identifier] = function[keyword](width, height)[parameters]
+
+ using arrow function notation
+
+ e.g.
+ var calculateArea = (width, height) => {};
+
+ concise arrow notation
+
+ e.g.
+ var sumNumbers = number => number + number;
+
+ vs.
+
+ var sumNumbers = number => {
+ var sum = number + number;
+ return sum;
+ };
+
+
+ */
