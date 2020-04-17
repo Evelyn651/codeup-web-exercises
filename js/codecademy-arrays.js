2535320c2ca610f661c12681a496b1dc91cb8408
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-16 19:52:45 -0500

    practicing arrays, trying to trouble shoot weather map project comparing to weather api file form lecture

---
+++ js/codecademy-arrays.js
@@ -0,0 +1,12 @@
+//1 ARRAYS
+
+let newYearsResolutions = ['keep a journal', 'takes a falconry class', 'learn to juggle'];
+console.log(newYearsResolutions);
+
+//2 Create an Array
+
+const hobbies = ["crocheting", "reading", "hiking"];
+console.log(hobbies);
+
+//3 Accessing Elements
+
