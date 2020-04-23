2571a0bb546b63fdedd73850442be2a29291806f
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-04-23 15:24:58 -0500

    exercise walk through with douglas

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,6 +35,41 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+//2
+let userLanguages = users.filter(function (user) {
+    //console.log(user.languages.length); //gives all the amounts of languages for each object.
+    return user.languages.length >= 3;
+});
+console.log(userLanguages); //gives only the ones with three or more languages
 
-let userLanguages = users.filter(users.languages);
-console.log(userLanguages);
+//3
+const userEmails = users.map(user => user.email);
+console.log(userEmails);
+
+//4
+const userYears = users.reduce((totalYears, user) => {
+    return totalYears + user.yearsOfExperience;
+}, 0);
+
+console.log('Total years: ', userYears);
+
+//5
+const longestEmail = users.reduce((longest, user) => {
+    if (user.email.length > longest.length) {
+        return user.email;
+    } else {
+        return longest;
+    }
+}, '');
+console.log(longestEmail);
+
+//6
+const everyone = users.reduce((names, user) => {
+    if(names !== ''){
+        names += ', '
+    }
+    return names + user.name;
+}, '');
+
+console.log(`Your instructors are: ${everyone}`);
+console.log('using join', users.map(user => user.name).join(', '));
